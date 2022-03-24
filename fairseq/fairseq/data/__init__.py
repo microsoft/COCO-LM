92ef82ff5ac48b13b6f8bf1904919f274c180942
Yu Meng <yumeng5@illinois.edu>
2022-03-24 12:00:18 -0700

    remove unused module import

--- fairseq/fairseq/data/__init__.py
+++ fairseq/fairseq/data/__init__.py
@@ -31,7 +31,6 @@ from .list_dataset import ListDataset
 from .lm_context_window_dataset import LMContextWindowDataset
 from .lru_cache_dataset import LRUCacheDataset
 from .mask_tokens_dataset import MaskTokensDataset
-from .mask_tokens_dataset2 import MaskTokensDataset2
 from .monolingual_dataset import MonolingualDataset
 from .multi_corpus_sampled_dataset import MultiCorpusSampledDataset
 from .nested_dictionary_dataset import NestedDictionaryDataset
